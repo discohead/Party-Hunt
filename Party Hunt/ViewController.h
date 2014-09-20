ba68f991b61a03eaf47b623aff3ce6ea034ec731
Jared McFarland <jaredcolin@gmail.com>
2014-09-20 14:50:28 -0700

    Initial Commit

---
+++ Party Hunt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Party Hunt
+//
+//  Created by Jared McFarland on 9/20/14.
+//  Copyright (c) 2014 Jared Colin McFarland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
